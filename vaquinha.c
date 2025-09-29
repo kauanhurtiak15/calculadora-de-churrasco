d4b569ce6ab50778d3ac215ec5941058577da3d6
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-09-29 13:49:48 +0000

    em teste

---
+++ vaquinha.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+
+void mostraResumo(int qtdPessoas){
+    printf("\n===== RESUMO CHURRASCO =====\n");
+    printf("Participantes: %d\n",qtdPessoas);
+}
+
+
+
+
+
+
+
+int main(){
+    int pessoas;
+    float valor;
+
+    printf("Digite o número de participantes: ");
+    scanf("%d",&pessoas);
+
+}

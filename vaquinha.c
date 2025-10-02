d00e73c0c829ede22753136886c70ee804b4fea3
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-10-02 11:47:09 +0000

    em teste

--- vaquinha.c
+++ vaquinha.c
@@ -10,14 +10,19 @@ void adicionaContribuicao(float valor){
 
 // Função: calcula a quantidade de carne por pessoa (0.5kg por pessoa)
 
-
+float calcularCarne(int qtdPessoas){
+    return qtdPessoas * 0.5;
+}
 
 
 
 // Procedimento para mostrar o resumo
 void mostraResumo(int qtdPessoas){
+    float carne = calcularCarne(qtdPessoas);
+    float media = totalArrecadado / qtdPessoas;
     printf("\n===== RESUMO CHURRASCO =====\n");
     printf("Participantes: %d\n",qtdPessoas);
+    printf("Carne necessária: 2");
 }
 
 
